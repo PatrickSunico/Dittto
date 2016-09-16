2a0a0d272c858f9dfbf8f932bf2ab5abcf8887c5
PatrickSunico <apatricksunico@gmail.com>
2016-09-16 17:39:52 +0800

    Defaults

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,14 @@ class ApplicationController < ActionController::Base
       # sanitizes the sign_up form then appends :username the one we added via rails migration
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:userhandle])
+
 
       # sanitizes the account_update form then appends :username the one we added via rails migration
       devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:userhandle])      
       devise_parameter_sanitizer.permit(:account_update, keys: [:occupation])
       devise_parameter_sanitizer.permit(:account_update, keys: [:company])
       devise_parameter_sanitizer.permit(:account_update, keys: [:location])
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :userhandle
+
   # If User Deletes Account Remove all post and comments associated with the user
   has_many :posts, :dependent => :destroy
   has_many :comments, :dependent => :destroy
