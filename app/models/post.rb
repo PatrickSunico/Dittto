b1728940a775a7290fb55762b5a951dceec81d82
PatrickSunico <apatricksunico@gmail.com>
2016-09-17 14:53:43 +0800

    Update Friendly Id

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,12 +8,17 @@ class Post < ApplicationRecord
   has_many :comments, :dependent => :destroy
   has_many :attachments, :dependent => :destroy
   process_in_background :thumbnail
-  friendly_id :slug_candidates, use: [:slugged, :finders]
+  friendly_id :should_generate_new_friendly_id?, use: [:slugged, :history]
+  friendly_id :slug_candidates, use: [:finders]
+
+  def should_generate_new_friendly_id?
+    slug.blank? || self.title_changed?
+  end
 
   def slug_candidates
-     [
-       :title,
-       [:title, :id]
-     ]
+    [
+      :title,
+      [:title, :id]
+    ]
   end
 end
