852a6f121dd593a94f657e21f85542bb8beef2de
PatrickSunico <apatricksunico@gmail.com>
2016-09-20 10:32:52 +0800

    Update Authorization

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,17 +1,17 @@
 
 class AttachmentsController < ApplicationController
-  before_action :set_upload, :only => [:update, :destroy]
+  before_action :set_upload, :only => [:new, :create ,:destroy]
   before_action :authorize_user, except: [:index, :show]
   def new
-    post = Post.find(params[:post_id])
-    @attachment = post.attachments.build
+    # @post = Post.friendly.find(params[:post_id])
+    @attachment = @post.attachments.build
     respond_to do |format|
       format.html
     end
   end
 
   def create
-    @post = Post.find(params[:post_id])
+    # @post = Post.friendly.find(params[:post_id])
     @attachment = @post.attachments.create(attachment_params)
 
     respond_to do |format|
@@ -25,24 +25,8 @@ class AttachmentsController < ApplicationController
     end
   end
 
-  def edit
-    post = Post.find(params[:id])
-    @attachment = Attachment.find(params[:post_id])
-  end
-
-  def update
-    respond_to do |format|
-      if @attachment.update(attachment_params)
-        format.html { redirect_to post_path(@post), notice: "Attachment was Successfully updated"}
-        format.json { render json: { message: "success", fileID: @attachment.id}, :status => 200}
-      else
-        format.html {render :edit}
-        format.json { render json: { error: @attachment.error.full_message.join(',')}, :status => 400}
-      end
-    end
-  end
-
   def destroy
+    @attachment = @post.attachments.find(params[:id])
     respond_to do |format|
       if @attachment.destroy
         format.html {redirect_to post_path(@post), notice: "Attachment was Sucessfully destroyed"}
@@ -53,8 +37,7 @@ class AttachmentsController < ApplicationController
 
   private
     def set_upload
-      @post = Post.find(params[:post_id])
-      @attachment = @post.attachments.find(params[:id])
+      @post = Post.friendly.find(params[:post_id])
     end
 
     def attachment_params
@@ -62,27 +45,18 @@ class AttachmentsController < ApplicationController
     end
 
     def authorize_user
-      user = User.find_by_id(params[:user_id])
-      post = Post.find_by_id(params[:post_id])
+      post = Post.friendly.find(params[:post_id])
+      # user = User.where(post_id: post)
       attachment = Attachment.find_by_id(params[:post_id])
-
 
       if current_user.nil?
         redirect_to new_user_session_path
       else
-        # if post.user_id != current_user.id
-        #   # if post.id != attachment.post_id
-        #     redirect_to root_path
-        #   # end
-        # end
-        # @post && @post.user != current_user
         if post && post.user != current_user
           if post.user != attachment
-            # Or make it as a route
-            render "shared/error"
+            redirect_to root_path
           end
         end
-
       end
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
   acts_as_votable
   validates :title, :description, :thumbnail, presence: true
   mount_uploader :thumbnail, ThumbnailUploader
@@ -8,4 +9,12 @@ class Post < ApplicationRecord
   has_many :comments, :dependent => :destroy
   has_many :attachments, :dependent => :destroy
   process_in_background :thumbnail
+
+  def slug_candidates
+    [
+      :title,
+      [:title, :id]
+    ]
+  end
+
 end
